47fc3dcd676f96935acee32f2010f496f93a663f
xrito <you@example.com>
2025-03-31 03:55:08 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => section.classList.add("visible"));
+});
